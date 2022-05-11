b6becf6705e92037b7757eb96902acdad0a3446d
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-05-11 13:26:09 -0500

    Haciendo uso de nuestros setters y getters de nuestras clases Persona, Cliente y Trabajador

--- IntroduccionJava/src/EjerciciosTema9/persona.java
+++ IntroduccionJava/src/EjerciciosTema9/persona.java
@@ -2,9 +2,50 @@ package EjerciciosTema9;
 
 public class persona {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
 
+        // Creacion de mis objetos
+        Persona persona = new Persona();
+        Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
+
+        // asignamos la informacion a nuestro objeto persona
+        persona.setName("Miguel");
+        persona.setAge(23);
+        persona.setPhone(333391968);
+
+        // asignamos la informacion a nuestro objeto cliente
+        cliente.setName("Valente");
+        cliente.setAge(22);
+        cliente.setPhone(333391967);
+        cliente.setCredito(1300);
+
+        // asignamos la informacion a nuestro objeto trabajador
+        trabajador.setName("Victor");
+        trabajador.setAge(21);
+        trabajador.setPhone(333391966);
+        trabajador.setSalario(1500);
+
+
+        // imprimimos la informacion con nuestros getters
+        System.out.println("\nLa informacion de nuestro objeto Persona");
+        System.out.println(persona.getName());
+        System.out.println(persona.getAge());
+        System.out.println(persona.getPhone());
+        System.out.println("\n");
+
+        System.out.println("La informacion de nuestro objeto Cliente");
+        System.out.println(cliente.getName());
+        System.out.println(cliente.getAge());
+        System.out.println(cliente.getPhone());
+        System.out.println(cliente.getCredito());
+        System.out.println("\n");
+
+        System.out.println("La informacion de nuestro objeto Trabajador");
+        System.out.println(trabajador.getName());
+        System.out.println(trabajador.getAge());
+        System.out.println(trabajador.getPhone());
+        System.out.println(trabajador.getSalario());
+    }
 
     // creacion de nuestra clase persona
     static class Persona {
