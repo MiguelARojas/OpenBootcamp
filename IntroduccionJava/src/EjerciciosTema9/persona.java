d69d672d3dbf9a0360b5ae1d9e96e85dab70d36d
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-05-11 13:14:22 -0500

    Declaracion de nuestros setters y getters de nuestra clase persona

--- IntroduccionJava/src/EjerciciosTema9/persona.java
+++ IntroduccionJava/src/EjerciciosTema9/persona.java
@@ -4,4 +4,42 @@ public class persona {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
     }
+
+
+    // creacion de nuestra clase persona
+    static class Persona {
+
+        // declaracion de nuestras varibles privadas
+        private int age;
+        private String name;
+        private int phone;
+
+
+        // declaracion de nuestros setters
+        public void setAge(int age){
+            this.age = age;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public void setPhone(int phone){
+            this.phone = phone;
+        }
+
+
+        // declaracion de nuestros getters
+        public int getAge(){
+            return this.age;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+
+        public int getPhone(){
+            return this.phone;
+        }
+    }
 }
